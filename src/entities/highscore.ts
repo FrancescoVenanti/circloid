b931c9bfa688d7dc28a18f815cb89e44ed1c86b9
FrancescoVenanti <francesco.venant@gmail.com>
2025-02-20 14:45:34 +0100

    highscore

--- src/entities/highscore.ts
+++ src/entities/highscore.ts
@@ -7,24 +7,42 @@ import Vector from "../core/vector";
 class Highscore extends Entity<any> {
   private scores: any[] = [];
   constructor(zIndex: number) {
-    super(zIndex, new Rect(Canvas.instance.rect.topLeft, 100, 200));
+    super(
+      zIndex,
+      new Rect(
+        Canvas.instance.rect.topLeft.clone().addX(60).addY(140),
+        120,
+        200
+      )
+    );
     this.loadScores();
   }
 
   private async loadScores() {
     const res = await fetch("/score", { method: "get" });
     this.scores = await res.json();
+    console.log(this.scores);
   }
 
   public draw(): void {
     const vect = this.shape.vector.clone();
-    for (const score in this.scores) {
-      Drawer.instance.with(() => this.drawScore(score, vect.addY(100)), {});
+    for (let i = 0; i < this.scores.length; i++) {
+      Drawer.instance.with(() => this.drawScore(vect.addY(40), i), {
+        fillStyle: "white",
+      });
     }
   }
 
-  private drawScore(score: any, vect: Vector) {
-    Drawer.instance.text(JSON.stringify(score), vect);
+  private drawScore(vect: Vector, index: number) {
+    Drawer.instance.text(
+      index +
+        1 +
+        ": " +
+        this.scores[index]["name"] +
+        " - " +
+        this.scores[index]["score"],
+      vect
+    );
   }
 
   public override update() {}
