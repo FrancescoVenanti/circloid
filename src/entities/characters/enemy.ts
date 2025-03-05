511e22b4847fff7d9429dc42b759e2fa011bbdf3
FrancescoVenanti <francesco.venant@gmail.com>
2025-03-05 17:29:20 +0100

    squares after 50

--- src/entities/characters/enemy.ts
+++ src/entities/characters/enemy.ts
@@ -4,26 +4,37 @@ import Shape from "@/src/core/shape/shape";
 import BallEnemy from "./ball-enemy";
 import GlobalMixin from "@/src/mixins/global";
 import SquareEnemy from "./square-enemy";
+import GLOBAL from "@/src/core/global";
 
 export interface IEnemy<T extends Shape> extends IEntity<T> {}
 
 class Enemy extends GlobalMixin(class {}) {
   private static enemies: ((
     speedMultiplier: number
-  ) => MovingEntity<any> | null)[] = [BallEnemy.spawn, SquareEnemy.spawn];
+  ) => MovingEntity<any> | null)[] = [BallEnemy.spawn];
   constructor() {
     super();
   }
 
   static spawn(speedMultiplier: number) {
+    this.addEnemies();
     const rand = Math.floor(Math.random() * this.enemies.length);
     const entity = this.enemies[rand](speedMultiplier);
+
     console.log(entity);
     if (entity != null) entity.store();
   }
   static spawnAmount() {}
 
-  public update(): void {}
+  static addEnemies() {
+    const player = GLOBAL.use("player");
+    if (!player) return;
+    if (player.points > 50) {
+      this.enemies.push(SquareEnemy.spawn);
+    }
+  }
+
+  static update(): void {}
 }
 
 export default Enemy;
